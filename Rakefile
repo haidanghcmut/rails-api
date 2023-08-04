b8df92dadc796118f8c062ba9e210a24afa22c85
haidang <dang.vohaidang@hcmut.edu.vn>
2023-08-04 17:53:24 +0700

    started-with-controller

--- Rakefile
+++ Rakefile
@@ -2,6 +2,4 @@
 # for example lib/tasks/capistrano.rake, and they will automatically be available to Rake.
 
 require_relative 'config/application'
-require 'rubocop/rake_task'
-RuboCop::RakeTask.new
 Rails.application.load_tasks
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  namespace :api do
+    resources :users do
+      collection do
+         post 'created with login'
+      end
+    end
+  end
 end
